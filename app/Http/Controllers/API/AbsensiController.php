40ba0cc55c1f7b6ca112f96efbf1482309df6c37
ahmadimamtnt <ahmadimamtnt@gmail.com>
2021-09-27 12:36:00 +0700

    making API for acces client public

---
+++ app/Http/Controllers/API/AbsensiController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Absensi;
+use Illuminate\Support\Facades\DB;
+
+class AbsensiController extends Controller
+{
+    function get()
+    {
+        $data = Absensi::all();
+        return response()->json(
+            $data
+        );
+    }
+
+    function getById($id_absensi)
+    {
+        $absensi = Absensi::where('id_absensi', $id_absensi)->get();
+
+        $count_data = $absensi->count();
+
+        if ($count_data == 0) {
+            return response()->json(
+                [
+                    "message" => "Data tidak ditemukan",
+                ],
+            );
+        } else {
+            return response()->json(
+                $absensi
+            );
+        }
+    }
+
+    function put($id_absensi, Request $request)
+    {
+        // get data from client
+        $id_pertemuan = $request->id_pertemuan;
+        $qrcode = $request->qrcode;
+        $imei = $request->imei;
+        $keterangan = $request->keterangan;
+        $updated_at = date('Y-m-d H:i:s');
+
+        $absensi = Absensi::where('id_absensi', $id_absensi)->first();
+
+        $cek_value_absensi = Absensi::where('id_absensi', $id_absensi)->value('keterangan');
+
+        $cek_qrcode = DB::table('absensis')
+            ->where('id_absensi', $id_absensi)
+            ->where('qrcode', $qrcode)
+            ->first();
+
+        $cek_imei = DB::table('absensis')
+            ->where('id_absensi', $id_absensi)
+            ->where('qrcode', $qrcode)
+            ->where('imei_absensi', $imei)
+            ->first();
+
+        if ($cek_value_absensi == null) {
+            if ($absensi) {
+                if ($cek_qrcode) {
+                    if ($cek_imei) {
+
+                        $absensi->keterangan = 'hadir';
+                        $absensi->save();
+
+                        return response()->json(
+                            [
+                                "message" => "Anda telah mengisi kehadiran",
+                                "data" => $absensi
+                            ]
+                        );
+                    } else {
+                        return response()->json(
+                            [
+                                "message" => "Perangkat tidak terdaftar di sistem"
+                            ]
+                        );
+                    }
+                }
+                return response()->json(
+                    [
+                        "message" => "QR Code tidak ditemukan"
+                    ]
+                );
+            }
+            return response()->json(
+                [
+                    "message" => "Gagal mengirim data. data tidak dikenal"
+                ]
+            );
+        }
+        return response()->json(
+            [
+                "message" => "Anda sudah melakukan absensi. Absensi hanya dapat dilakukan satu kali."
+            ]
+        );
+        
+    }
+}
---
+++ app/Http/Controllers/API/ParticipantController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Participant;
+use Illuminate\Support\Facades\DB;
+
+class ParticipantController extends Controller
+{
+    function post(Request $request)
+    {
+        // created_at & updated_at
+        $created_at = date('Y-m-d H:i:s');
+        $updated_at = date('Y-m-d H:i:s');
+
+        // get data from client
+        $id_seksi = $request->id_seksi;
+        $user_id = $request->user_id;
+        $imei = $request->imei;
+
+        $hasil_request = DB::table('participants')
+            ->where('id_seksi', '=', $id_seksi)
+            ->where('user_id', '=', $user_id)
+            ->count();
+
+        if ($hasil_request == 0) {
+            $data = [
+                'id_seksi' => $request->id_seksi,
+                'user_id' => $request->user_id,
+                'imei_participant' => $imei,
+                'keterangan' => '0',
+                'created_at' => $created_at,
+                'updated_at' => $updated_at
+            ];
+
+            Participant::create($data);
+
+            return response()->json(
+                [
+                    "message" => "Selamat, anda berhasil bergabung.",
+                    "data" => $data
+                ]
+            );
+        } else {
+            return response()->json(
+                [
+                    "message" => "Maaf, anda sudah bergabung di kelas ini."
+                ]
+            );
+        }
+    }
+
+    function get()
+    {
+        $data = Participant::all();
+        return response()->json(
+            $data
+        );
+    }
+
+    function getById($id_participant)
+    {
+        $data = Participant::where('id_participant', $id_participant)->get();
+
+        $count_data = $data->count();
+
+        if ($count_data == 0) {
+            return response()->json(
+                "Data tidak ditemukan"
+            );
+        } else {
+            return response()->json(
+                $data
+            );
+        }
+    }
+}
