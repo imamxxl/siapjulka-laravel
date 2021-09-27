d27eacba911f11308c5a1b390a5ecabb0bbc4b72
ahmadimamtnt <ahmadimamtnt@gmail.com>
2021-09-27 16:06:03 +0700

    created route for api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AbsensiController;
 use App\Http\Controllers\API\ParticipantController;
+use App\Http\Controllers\API\MahasiswaController;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\SeksiController;
+use App\Http\Controllers\API\PertemuanController;
+use App\Http\Controllers\API\JurusanController;
+use App\Http\Controllers\API\RuangController;
 use App\Models\Mahasiswa;
 
 /*
@@ -32,4 +38,18 @@ Route::get('/absensi/{id_absensi}', [AbsensiController::class, 'getById']);
 Route::put('/absensi/{id_absensi}', [AbsensiController::class, 'put']);
 
 // For Mahasiswa
-Route::get('/mahasiswa', [MahasiswaController::class, 'get']);
+Route::get('/mahasiswas', [MahasiswaController::class, 'get']);
+
+// For Users
+Route::get('/users', [UserController::class, 'get']);
+
+// For Seksis
+Route::get('/seksis', [SeksiController::class, 'get']);
+
+// For Pertemuans
+Route::get('/pertemuans', [PertemuanController::class, 'get']);
+
+// For Pertemuans
+Route::get('/jurusans', [JurusanController::class, 'get']);
+
+Route::get('/ruangs', [RuangController::class, 'get']);
