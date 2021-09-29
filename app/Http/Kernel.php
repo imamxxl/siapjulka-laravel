c9191fae57df257c6f7d9c844439b9716fac52b2
ahmadimamtnt <ahmadimamtnt@gmail.com>
2021-09-29 22:57:27 +0700

    updating kernel with adding role admin, dosen, mahasiswa

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\Admin;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -62,5 +63,8 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\Admin::class,
+        'dosen' => \App\Http\Middleware\Dosen::class,
+        'mahasiswa' => \App\Http\Middleware\Mahasiswa::class,
     ];
 }
